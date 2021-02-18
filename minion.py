a4a72b0e324d564fd8378cd5b7aa9b605d359d0b
Alex Clark <aclark@aclark.net>
2021-02-18 16:10:24 -0500

    Update

--- minion.py
+++ minion.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
 from sys import argv
 
-# Kevin and Stuart want to play the 'The Minion Game'.
-# 
-# Game Rules
-# 
-# Both players are given the same string, .
-# Both players have to make substrings using the letters of the string .
-#
-# Stuart has to make words starting with consonants.
-# Kevin has to make words starting with vowels.
-#
-# The game ends when both players have made all possible substrings.
-#  
-# Scoring
-# A player gets +1 point for each occurrence of the substring in the string.
-#  
-# For Example:
-#
-# String  = BANANA
-# Kevin's vowel beginning word = ANA
-#
-# Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
-
-
-def minion(word):
+def make_substring(word):
     """
-    """
+Kevin and Stuart want to play the 'The Minion Game'.
 
+Game Rules
+
+Both players are given the same string, .
+Both players have to make substrings using the letters of the string .
+
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+
+The game ends when both players have made all possible substrings.
+ 
+Scoring
+A player gets +1 point for each occurrence of the substring in the string.
+ 
+For Example:
+
+String  = BANANA
+Kevin's vowel beginning word = ANA
+
+Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+    """
+    print(word)
 
 if __name__ == "__main__":
     if len(argv) == 2:
+        make_substring(argv[1])
         exit(0)
     else:
         print("Usage: %s <word>" % argv[0])
