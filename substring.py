863c3b14a7774fa658c2dbcc744e46754389437e
Alex Clark <aclark@aclark.net>
2021-02-18 16:21:16 -0500

    Update

--- substring.py
+++ substring.py
@@ -3,6 +3,9 @@ from sys import argv
 
 def substring(word):
     """
+
+---
+
 Kevin and Stuart want to play the 'The Minion Game'.
 
 Game Rules
@@ -26,7 +29,39 @@ Kevin's vowel beginning word = ANA
 
 
 Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
-    """
+
+---
+
+Alice and Bob want to play the following game:
+
+* Game Rules
+
+Both players are given the same string composed only of capital letters.
+
+Both players have to make substrings using the letters of the string. 
+
+    Alice has to make words starting with consonants.
+    Bob has to make words starting with vowels.
+
+The game ends when both players have made all possible substrings.
+
+* Scoring
+
+A player gets +1 point for each unique substring.
+
+* Example
+
+SCIENCE
+
+Alice (17)
+    S, C, N, SC, CI, NC, CE, SCI, CIE, NCE, SCIE, CIEN, SCIEN, CIENC, SCIENC, CIENCE, SCIENCE
+    
+Bob (9)
+    I, E, EI, EN, IEN, ENC, IENC, ENCE, IENCE
+    
+* Write a function game_outcome(word) that returns a string with the winner and their score, e.g. 'Alice 17', or 'Draw' if there is a draw. 
+
+"""
     print(word)
 
 
